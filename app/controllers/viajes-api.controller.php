fe9a490caa301782eb2d8c16ed22fc580f36d89f
lloysmaster <alejo_facundo@yahoo.com>
2026-06-20 18:53:10 -0300

    funcionando parcialmente

--- app/controllers/viajes-api.controller.php
+++ app/controllers/viajes-api.controller.php
@@ -6,19 +6,16 @@ class ViajesApiController {
 
     public function  __construct() {
         $this->model = new ViajesModel();
-
-        // no hay vista en la API REST
     }
 
     public function getViajes($req, $res) {
         $status = $req->query->status ?? null;
 
         if ($status) {
-            // TODO: verificar que el estado sea válido -> sino Bad Request 400
-            
+            // verificar que el estado sea válido -> sino Bad Request 400
             $issues = $this->model->getAllByStatus($status);
         } else {
-            $issues = $this->model->getAll();
+            $issues = $this->model->getViajes();
         }
 
         // respondo issues con 200 OK
@@ -29,7 +26,7 @@ class ViajesApiController {
         // obtengo el ID que viene como parámetro del endpoint
         $id_viaje = $req->params->id;
 
-        $viaje = $this->model->get($id_viaje);
+        $viaje = $this->model->getViaje($id_viaje);
 
         if (!$viaje) {
             return $res->json("El viaje con el id=$id_viaje no existe", 404);
@@ -39,19 +36,20 @@ class ViajesApiController {
     }
 
     public function insertViaje($req, $res) {
-        $title = $req->body->title ?? null;
-        $description = $req->body->description ?? null;
-        $assigned = $req->body->assigned ?? null;
-        $type = $req->body->type ?? null;
-        $status = $req->body->status ?? 'TODO';
+        //[$nombre_ciudad, $pais, $descripcion, $precio, $id]
+        $nombre_ciudad = $req->body->title ?? null;
+        $pais = $req->body->pais ?? null;
+        $descripcion = $req->body->description ?? null;
+        $precio = $req->body->precio ?? null;
+        $id = $req->body->id ?? null;
 
         // Valida que vengan todos los datos necesarios en el body
         // Si falta alguno, devolvemos un error 400 (Bad Request)
-        if (empty($title) || empty($description) || empty($type)) {
+        if (empty($nombre_ciudad) || empty($pais) || empty($descripcion) || empty($precio)) {
             return $res->json('Falta completar datos', 400);
         }
 
-        $id = $this->model->insert($title, $description, $assigned, $type, $status);
+        $id = $this->model->insertViaje($nombre_ciudad, $pais, $descripcion, $precio, $id);
 
         // si el modelo devuelve false, algo falló al guardar (por ejemplo, error en la base de datos)
         if (!$id) {
@@ -60,45 +58,44 @@ class ViajesApiController {
 
         // se considera una buena práctica devolver la entidad creada que contiene
         // todos los datos que el modelo agregó automaticamente
-        $viaje = $this->model->get($id);
+        $viaje = $this->model->getViaje($id);
         return $res->json($viaje, 201);
     }
 
     public function removeViaje($req, $res) {
         $id_viaje = $req->params->id;
-        $viaje = $this->model->get($id_viaje);
+        $viaje = $this->model->getViaje($id_viaje);
 
         if (!$viaje) {
             return $res->json("El viaje con el id=$id_viaje no existe", 404);
         }
 
-        $this->model->delete($id_viaje);
+        $this->model->deleteViaje($id_viaje);
         return $res->json("El viaje con el id=$id_viaje se eliminó", 200);
     }
 
     public function updateViaje($req, $res) {
         $id_viaje = $req->params->id;
-        $viaje = $this->model->get($id_viaje);
+        $viaje = $this->model->getViaje($id_viaje);
 
         if (!$viaje) {
             return $res->json("El viaje con el id=$id_viaje no existe", 404);
         }
-
-        $title = $req->body->title ?? null;
-        $description = $req->body->description ?? null;
-        $assigned = $req->body->assigned ?? null;
-        $type = $req->body->type ?? null;
-        $status = $req->body->status ?? 'TODO';
+        //[$nombre_ciudad, $pais, $descripcion, $precio, $id]
+        $nombre_ciudad = $req->body->title ?? null;
+        $pais = $req->body->pais ?? null;
+        $descripcion = $req->body->description ?? null;
+        $precio = $req->body->precio ?? null;
 
         // Valida que vengan todos los datos necesarios en el body
         // Si falta alguno, devolvemos un error 400 (Bad Request)
-        if (empty($title) || empty($description) || empty($type)) {
+        if (empty($nombre_ciudad) || empty($pais) || empty($descripcion) || empty($precio)) {
             return $res->json('Falta completar datos', 400);
         }
 
-        $this->model->update($id_viaje, $title, $description, $assigned, $type, $status);
+        $this->model->updateViaje($id_viaje, $nombre_ciudad, $pais, $descripcion, $precio);
         
-        $viaje = $this->model->get($id_viaje);
+        $viaje = $this->model->getViaje($id_viaje);
         return $res->json($viaje, 200);
     }
 
--- router.php
+++ router.php
@@ -6,8 +6,8 @@ require_once 'app/controllers/viajes-api.controller.php';
 $router = new Router();
 
 // define la tabla de ruteo
-$router->addRoute('viajes', 'GET', 'viajes-api.controller', 'getViajes');
-$router->addRoute('viajes/:id', 'GET', 'viajes-api.controller', 'getViajeById');
+$router->addRoute('viajes', 'GET', 'ViajesApiController', 'getViajes');
+$router->addRoute('viajes/:id', 'GET', 'ViajesApiController', 'getViajeById');
 $router->addRoute('viajes/:id', 'DELETE', 'viajes-api.controller', 'removeViaje');
 $router->addRoute('viajes', 'POST', 'viajes-api.controller', 'insertViaje');
 $router->addRoute('viajes/:id', 'PUT', 'viajes-api.controller', 'updateViaje');
